9748328a8cde91d8629caa56678ebc509d7c7bdc
debleenasarkar <ria.debleena@gmail.com>
2024-04-07 19:49:03 +0000

    m1

---
+++ src/main/java/simplilearn/example/demotablet/DemoTabletApplication.java
@@ -0,0 +1,13 @@
+package simplilearn.example.demotablet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTabletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTabletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/simplilearn/example/demotablet/DemoTabletApplicationTests.java
@@ -0,0 +1,13 @@
+package simplilearn.example.demotablet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTabletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
